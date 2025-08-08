806d1aafe72a2b33bbb9f099c5132013a7132f99
kijikoji <cvillagr97@gmail.com>
2025-08-07 19:28:49 -0500

    upload/download nonworking

--- 02-file-explorer-api/index.js
+++ 02-file-explorer-api/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const multer = require('multer');
 const fs = require('fs').promises;
 const path = require('path');
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const dirPath = req.body.path || './uploads';
+    fs.mkdir(dirPath, { recursive: true })
+      .then(() => cb(null, dirPath))
+      .catch(err => cb(err)); 
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -26,6 +41,14 @@ app.get('/api/list', async (req, res) => {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
+});
+
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  console.log('file saved at ', req.file.path);
+  res.json({
+    message: 'File uploaded successfully',
+    file: req.file,
+  });
 });
 
 app.listen(PORT, () => {
